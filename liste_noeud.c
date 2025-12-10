c1e799d89561062c50e27c4d788a61501b2ff6f5
fkn2473 <fkn2473@maya.enseeiht.fr>
2025-12-10 17:48:17 +0100

    Début d'implémentation.

--- liste_noeud.c
+++ liste_noeud.c
@@ -3,6 +3,58 @@
 #include <stdlib.h>
 #include <math.h>
 
-/* TODO */
+LCA creer_liste () {
+    return Null; /* A revoir car ça ne respecte pas les post-condtions. */
+}
+
+
+void detruire_liste (LCA liste_ptr) {
+    LCA Courant = liste_ptr;
+    while (Courant != Null) {
+        Free(Courant);
+        Courant := Courant.Suivant;
+    }
+}
+
+
+bool est_vide_liste () {
+    LCA Courant = liste_ptr;
+    
+}
+
+
+bool contient_noeud_liste () {
+
+}
+
+
+bool contient_arrete_liste () {
+
+}
+
+
+int cout_noeud_liste () {
+
+}
+
+
+LCA precedent_noeud_liste () {
+
+}
+
+
+LCA min_noeud_liste () {
+
+}
+
+
+void inserer_noeud_liste () {
+
+}
+
+
+void supprimer_noeud_liste () {
+
+}
 
 
