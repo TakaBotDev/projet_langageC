aac37b9efe75d775c12f43b2aba741211a233acc
saadlef <saadlefrais@gmail.com>
2025-12-31 20:39:18 +0100

    Update coord.c

--- coord.c
+++ coord.c
@@ -1,3 +1,18 @@
 #include "coord.h"
 
-// TODO : implanter le module coord
+coord_t creer_coord(int x, int y) {
+    coord_t resultat;
+    resultat.x = x;
+    resultat.y = y;
+    return resultat;
+}
+
+
+int get_x(coord_t position) {
+    return position.x;
+}
+
+
+int get_y(coord_t position) {
+    return position.y;
+}
