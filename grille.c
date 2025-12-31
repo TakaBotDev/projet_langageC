51ebbc20b90c998f43616d263417bf3acd3f5042
saadlef <saadlefrais@gmail.com>
2025-12-31 22:45:07 +0100

    grille.c complété

--- grille.c
+++ grille.c
@@ -33,3 +33,87 @@ grille_t creer_grille(int largeur, int profondeur) {
 }
 
 
+void detruire_grille(grille_t grille) {
+    free(grille.hauteurs);
+}
+
+
+bool dans_les_bornes(grille_t grille, coord_t position) {
+    int x = get_x(position);
+    int y = get_y(position);
+    return (x >= 0 && x < grille.largeur && y >= 0 && y < grille.profondeur);
+}    
+
+
+int get_largeur(grille_t grille) {
+    return grille.largeur;
+}
+
+
+int get_profondeur(grille_t grille) {
+    return grille.profondeur;
+}
+
+
+coord_t inferieur_gauche(grille_t grille) {
+    return creer_coord(0, 0);
+}
+
+
+coord_t superieur_droit(grille_t grille) {
+    return creer_coord(grille.largeur - 1, grille.profondeur - 1);
+}
+
+
+
+void set_hauteur(grille_t grille, coord_t position, float hauteur) {
+    assert(dans_les_bornes(grille, position));
+    grille.hauteurs[coord_lin(grille, position)] = hauteur;
+}
+
+
+float get_hauteur(grille_t grille, coord_t position) {
+    assert(dans_les_bornes(grille, position));
+    return grille.hauteurs[coord_lin(grille, position)];
+}
+
+
+size_t get_voisins(grille_t grille, coord_t position, float seuil, coord_t** voisins) {
+    assert(voisins != NULL);
+    assert(seuil >= 0.0f);
+
+    float hauteur_initiale = get_hauteur(grille, position);
+
+    int dx[] = {1, -1, 0, 0};
+    int dy[] = {0, 0, 1, -1};
+
+    coord_t* voisins_temp = (coord_t*)malloc(4 * sizeof(coord_t));
+    assert(voisins_temp != NULL);
+
+    size_t ind_voisin_valide = 0; 
+
+    for (int i = 0; i < 4; i++) {
+        coord_t voisin = translation(position, dx[i], dy[i]);
+
+        if (dans_les_bornes(grille, voisin)) {
+            float hauteur_voisin = get_hauteur(grille, voisin);
+            float denivele = fabsf(hauteur_voisin - hauteur_initiale);
+
+            if (denivele <= seuil) {
+                voisins_temp[ind_voisin_valide] = voisin;
+                ind_voisin_valide++;
+            }
+        }
+    }
+
+
+    if (ind_voisin_valide > 0) {
+        *voisins = (coord_t*)realloc(voisins_temp, ind_voisin_valide * sizeof(coord_t));
+        assert(*voisins != NULL);
+    } else {
+        free(voisins_temp);
+        *voisins = NULL;
+    }
+    
+    return nb_voisins;
+}   
