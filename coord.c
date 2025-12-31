dd32548fa5aed7b486f5ef3554ee4c7569779cd5
saadlef <saadlefrais@gmail.com>
2025-12-31 21:03:28 +0100

    coord.c codé

--- coord.c
+++ coord.c
@@ -1,4 +1,5 @@
 #include "coord.h"
+#include <math.h>
 
 coord_t creer_coord(int x, int y) {
     coord_t resultat;
@@ -15,4 +16,31 @@ int get_x(coord_t position) {
 
 int get_y(coord_t position) {
     return position.y;
+}
+
+
+void set_x(coord_t* position, int x) {
+    (*position).x = x;
+}
+
+
+void set_y(coord_t* position, int y) {
+    (*position).y = y;
+}
+
+
+bool memes_coord(coord_t c1, coord_t c2) {
+    return (c1.x == c2.x) && (c1.y == c2.y);
+}
+
+
+coord_t translation(coord_t position, int dx, int dy) {
+    return creer_coord(position.x + dx, position.y + dy);
+}
+
+
+float distance_euclidienne(coord_t p1, coord_t p2) {
+    int diffx = p2.x - p1.x;
+    int diffy = p2.y - p1.y;
+    return sqrtf( (float)(diffx * diffx + diffy * diffy) );
 }
